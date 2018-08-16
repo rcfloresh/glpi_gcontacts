a778495edbb63fef10db549694697d2fbb5dc0b6
rcfloresh <robertocarlos.floresh@gmail.com>
2018-08-16 11:35:02 -0600

    New language. Spanish

---
+++ locale/es.php
@@ -0,0 +1,17 @@
+
+<?php
+function GetLanguage(){
+    return array(
+        'main_label'           => 'Configuración',
+        'account_settings'     => 'Configuración de cuenta',
+        'delete_accaunt'       => 'Eliminar cuenta',
+        'update_accaunt'       => 'Configurar actualizaciones',
+        'sevres_settings'      => 'Configurar servicio',
+        'get_data'             => 'Obtener datos',
+        'save'                 => 'Guardar',
+        'new_accaunt'          => 'Agregar una nueva cuenta',
+        'sync'                 => 'Sincronizar GLPI con Google',
+        'sync_now'             => 'Hay una sincronización de contactos de GLPI a Google Contact ...',
+        'error'                => '¡No has configurado un servicio! Después de la configuración, las funciones principales estarán disponibles.'
+    );
+}
